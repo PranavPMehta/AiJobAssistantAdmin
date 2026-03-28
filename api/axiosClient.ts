ae5ddd6173ccebe7e48a038c2f79239f4b224c34
PranavPMehta <pranavm182@gmail.com>
2026-03-28 16:58:13 +0530

    Resolving the conflicts with earlier changes

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8090",
+  baseURL: "https://dheerajrathodconsult.com",
   headers: {
     "Content-Type": "application/json",
   },
--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://dheerajrathodconsult.com',
+    baseURL: 'https://dheerajrathodconsult.com',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
